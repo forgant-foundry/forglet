7a7b1cbf7434f16e0b6eb4dc1b6b36f37d78397e
Mark Egan <mark.egan@forgant.com>
2026-04-19 21:10:14 -0500

    adding gitplugin to main.go

--- cmd/forglet/main.go
+++ cmd/forglet/main.go
@@ -5,9 +5,11 @@ import (
 	"os"
 
 	"github.com/forgant-foundry/forglet/cmd/forglet/commands"
+	gitplugin "github.com/forgant-foundry/forglet/internal/plugins/git"
 )
 
 func main() {
+	commands.RegisterPlugin(gitplugin.New())
 	if err := commands.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
